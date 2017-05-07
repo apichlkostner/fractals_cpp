39417f5bbd9666cc71c06e86200c215777693bb9
Arthur Pichlkostner <a.pichlkostner@gmx.de>
2017-05-07 18:40:21 +0200

    First version, simple calculation of julia set.

---
+++ juliaset.cpp
@@ -0,0 +1,6 @@
+#include "juliaset.h"
+
+JuliaSet::JuliaSet()
+{
+
+}
---
+++ juliaset.h
@@ -0,0 +1,11 @@
+#ifndef JULIASET_H
+#define JULIASET_H
+
+
+class JuliaSet
+{
+public:
+    JuliaSet();
+};
+
+#endif // JULIASET_H
